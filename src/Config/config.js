c0e4048cd484d8170ee9df9d9857379d68266dac
sjcco <73748496+sjcco@users.noreply.github.com>
2021-07-14 16:48:34 -0500

    Import Scenes

---
+++ src/Config/config.js
@@ -0,0 +1,26 @@
+import Phaser from "phaser";
+import SceneBoot from "../Scenes/SceneBoot";
+import ScenePreloader from "../Scenes/ScenePreloader";
+import SceneTitle from "../Scenes/SceneTitle";
+import SceneGame from "../Scenes/SceneGame";
+ 
+export default {
+  type: Phaser.WEBGL,
+  width: 640,
+  height: 480,
+  backgroundColor: "white",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  scene: [
+    SceneBoot,
+    ScenePreloader,
+    SceneTitle,
+    SceneGame
+  ],
+  pixelArt: true,
+  roundPixels: true
+};
