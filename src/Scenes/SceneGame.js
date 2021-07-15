2143a4d858a44c8a75faa8b1c8f788771f1cbb5c
sjcco <73748496+sjcco@users.noreply.github.com>
2021-07-15 02:20:35 -0500

    Add logic for player movement + player animations

--- src/Scenes/SceneGame.js
+++ src/Scenes/SceneGame.js
@@ -1,7 +1,47 @@
 import Phaser from 'phaser';
+import Player from '../characters/player';
 
 export default class SceneGame extends Phaser.Scene {
   constructor() {
     super({ key: 'Game' });
+  }
+
+  setBackground() {
+    this.sky = this.add.image(this.game.config.width * 0.5, this.game.config.height * 0.5, 'sky');
+    this.sky.displayWidth = this.game.config.width;
+    this.sky.displayHeight = this.game.config.height;
+
+    this.farGround = this.add.image(this.game.config.width * 0.5, this.game.config.height - 90, 'far-ground');
+  }
+
+  create() {
+    this.setBackground();
+
+    this.ground = this.physics.add.staticImage(this.game.config.width * 0.5, 480, 'ground').refreshBody();
+
+    this.player = new Player(
+      this,
+      this.game.config.width * 0.5,
+      this.game.config.height * 0.5,
+    );
+
+    this.physics.add.collider(this.player, this.ground);
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    this.player.update();
+    if (this.cursors.left.isDown) {
+      this.player.moveLeft();
+    } else if (this.cursors.right.isDown) {
+      this.player.moveRight();
+    } else if (this.player.body.touching.down) {
+      this.player.idle();
+    }
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.jump();
+    } else if (this.cursors.down.isDown && this.player.body.touching.down) {
+      this.player.crouch();
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import Phaser from 'phaser';
+import './style.css';
 import config from './Config/config';
 
-
+const parent = document.createElement('div');
+parent.setAttribute('id', 'canvasContainer');
+parent.classList.add('container');
+document.body.appendChild(parent);
 // eslint-disable-next-line no-unused-vars
 const game = new Phaser.Game(config);
+const can = document.querySelector('canvas');
+console.log(game.canvas);
+console.log(can);
