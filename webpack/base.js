258e8ab6dbe9c49a3ca1e21d865d351e8c58d2cd
sjcco <73748496+sjcco@users.noreply.github.com>
2021-07-16 01:45:55 -0500

    Fix Assests for netlify

--- webpack/base.js
+++ webpack/base.js
@@ -50,7 +50,7 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: './src/assets/', to: './assets/' },
+        { from: './src/assets/', to: './dist/src/assets/' },
       ],
     }),
   ],
