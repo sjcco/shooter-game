c1945e34e87609e1bd945ac628c5d04fc7e2bd66
sjcco <73748496+sjcco@users.noreply.github.com>
2021-07-15 22:18:46 -0500

    Add characters able to die + refactoring of code

--- src/characters/enemies.js
+++ src/characters/enemies.js
@@ -1,49 +1,87 @@
 import Phaser from 'phaser';
 import character from './character';
+import Bullet from '../entities/bullets';
+import getAnimation from '../helpers/animationGetter';
 
 export default class Enemy extends character {
   constructor(scene, x, y, key) {
     super(scene, x, y, key);
     this.setData('speed', 150);
     this.setData('face-direction', 'right');
+    this.setData('character', 'enemy');
     this.anims.play('gunner-red-idle-right-anim', true);
+
+    this.shootTimer = this.scene.time.addEvent({
+      delay: 1500,
+      callback() {
+        let enemyBullet;
+        if (this.getData('face-direction') === 'right') {
+          enemyBullet = new Bullet(
+            this.scene,
+            this.x + 15,
+            this.y - 2,
+            250,
+          );
+        } else {
+          enemyBullet = new Bullet(
+            this.scene,
+            this.x - 15,
+            this.y - 2,
+            -250,
+          );
+        }
+        this.scene.enemyBullets.add(enemyBullet);
+      },
+      callbackScope: this,
+      loop: true,
+    });
   }
 
   moveLeft() {
     this.setData('face-direction', 'left');
+    this.body.setAcceleration(100);
     if (this.body.touching.down) {
       this.body.velocity.x = -this.getData('speed');
       this.anims.play('gunner-red-run-left-anim', true);
     } else {
       this.body.velocity.x = -100;
+      this.play('gunner-red-jump-left-anim', true);
     }
   }
 
   moveRight() {
     this.setData('face-direction', 'right');
+    this.body.setAcceleration(-100);
     if (this.body.touching.down) {
       this.body.velocity.x = this.getData('speed');
       this.anims.play('gunner-red-run-right-anim', true);
     } else {
       this.body.velocity.x = 100;
+      this.play('gunner-red-jump-right-anim', true);
     }
   }
 
   jump() {
     this.body.setVelocityY(-250);
-    if (this.getData('face-direction') === 'right') {
-      this.play('gunner-red-jump-right-anim', false);
-    } else if (this.getData('face-direction') === 'left') {
-      this.play('gunner-red-jump-left-anim', true);
-    }
+    this.play(
+      getAnimation(
+        this.getData('face-direction'),
+        this.getData('character'),
+        'jump',
+      ),
+      true,
+    );
   }
 
   idle() {
-    if (this.getData('face-direction') === 'right') {
-      this.play('gunner-red-idle-right-anim', true);
-    } else if (this.getData('face-direction') === 'left') {
-      this.play('gunner-red-idle-left-anim', true);
-    }
+    this.play(
+      getAnimation(
+        this.getData('face-direction'),
+        this.getData('character'),
+        'idle',
+      ),
+      true,
+    );
   }
 
   explode(canDestroy) {
@@ -51,15 +89,22 @@ export default class Enemy extends character {
       // Set the texture to the explosion image, then play the animation
       this.body.allowGravity = false;
       this.setTexture('Explosion'); // this refers to the same animation key we used when we added this.anims.create previously
-      this.play('Explosion'); // play the animation
+      this.play('Explosion', false); // play the animation
 
       // pick a random explosion sound within the array we defined in this.sfx in SceneMain
       this.scene.sfx.explosions[
         Phaser.Math.Between(0, this.scene.sfx.explosions.length - 1)
       ].play();
 
+      if (this.shootTimer !== undefined) {
+        if (this.shootTimer) {
+          this.shootTimer.remove(false);
+        }
+      }
+
       this.setAngle(0);
       this.body.setVelocity(0, 0);
+      this.body.setAcceleration(0, 0);
 
       this.on('animationcomplete', () => {
         if (canDestroy) {
@@ -74,6 +119,14 @@ export default class Enemy extends character {
   }
 
   update() {
-    this.body.velocity.x = 0;
+    if (!this.getData('isDead')) {
+      if (Math.abs(this.body.velocity.x) <= 10) {
+        this.body.velocity.x = 0;
+        this.body.acceleration.x = 0;
+        this.idle();
+      }
+    } else {
+      this.destroy();
+    }
   }
 }
--- src/characters/player.js
+++ src/characters/player.js
@@ -1,4 +1,6 @@
+import Phaser from 'phaser';
 import Bullet from '../entities/bullets';
+import getAnimation from '../helpers/animationGetter';
 import Character from './character';
 
 export default class Player extends Character {
@@ -8,8 +10,10 @@ export default class Player extends Character {
     this.setData('face-direction', 'right');
     this.anims.play('gunner-blue-idle-right-anim', true);
     this.setData('isShooting', false);
-    this.setData('timerShootDelay', 10);
+    this.setData('timerShootDelay', 50);
     this.setData('timerShootTick', this.getData('timerShootDelay') - 1);
+    this.setData('health', 5);
+    this.setData('character', 'player');
   }
 
   moveLeft() {
@@ -30,24 +34,61 @@ export default class Player extends Character {
       this.anims.play('gunner-blue-run-right-anim', true);
     } else {
       this.body.velocity.x = 100;
-      this.play('gunner-blue-jump-right-anim', false);
+      this.play('gunner-blue-jump-right-anim', true);
     }
   }
 
   jump() {
-    this.body.setVelocityY(-250);
-    if (this.getData('face-direction') === 'right') {
-      this.play('gunner-blue-jump-right-anim', false);
-    } else if (this.getData('face-direction') === 'left') {
-      this.play('gunner-blue-jump-left-anim', true);
-    }
+    this.body.setVelocityY(-270);
+    this.play(
+      getAnimation(
+        this.getData('face-direction'),
+        this.getData('character'),
+        'jump',
+      ),
+      true,
+    );
   }
 
   idle() {
-    if (this.getData('face-direction') === 'right') {
-      this.play('gunner-blue-idle-right-anim', true);
-    } else if (this.getData('face-direction') === 'left') {
-      this.play('gunner-blue-idle-left-anim', true);
+    this.play(getAnimation(this.getData('face-direction'), this.getData('character'), 'idle'), true);
+  }
+
+  hurt() {
+    this.setData('health', this.getData('health') - 1);
+    this.scene.sfx.hurt[0].play();
+  }
+
+  explode(canDestroy) {
+    if (!this.getData('isDead')) {
+      // Set the texture to the explosion image, then play the animation
+      this.body.allowGravity = false;
+      this.setTexture('Explosion'); // this refers to the same animation key we used when we added this.anims.create previously
+      this.play('Explosion'); // play the animation
+
+      // pick a random explosion sound within the array we defined in this.sfx in SceneMain
+      this.scene.sfx.explosions[
+        Phaser.Math.Between(0, this.scene.sfx.explosions.length - 1)
+      ].play();
+
+      if (this.shootTimer !== undefined) {
+        if (this.shootTimer) {
+          this.shootTimer.remove(false);
+        }
+      }
+
+      this.setAngle(0);
+      this.body.setVelocity(0, 0);
+
+      this.on('animationcomplete', () => {
+        if (canDestroy) {
+          this.destroy();
+        } else {
+          this.setVisible(false);
+        }
+      }, this);
+
+      this.setData('isDead', true);
     }
   }
 
