a0933d6b0ae2a5e22729a772002e4586d3d986f8
sjcco <73748496+sjcco@users.noreply.github.com>
2021-07-20 00:22:56 -0500

    Revert testing config

--- __tests__/sys/api.test.js
+++ __tests__/sys/api.test.js
@@ -1,5 +1,6 @@
-import { getRankings } from "../../src/sys/api";
+import { getRankings } from '../../src/sys/api';
 
 test('Should return an Array', () => {
+  // eslint-disable-next-line no-unused-expressions
   expect(Array.isArray(getRankings())).toBeTruthy;
 });
